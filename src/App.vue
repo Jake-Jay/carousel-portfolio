630a05d6e0e6448400ce320ff3d6791b6905ee21
Jake Pencharz <jake.jay.p@gmail.com>
2021-01-12 22:11:36 +0100

    Implement final slides and dark mode

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <v-app>
-    <v-app-bar flat color='white'>
+    <v-app-bar flat color='black'>
       <v-img
-        :src="require('./assets/TUM_logo.png')"
+        :src="require('./assets/TMS_Logo.png')"
         contain
         height="40"
         />
     </v-app-bar>
     <v-main>
-      <v-container>
+      <v-container >
         <Carousel/>
       </v-container>
     </v-main>
